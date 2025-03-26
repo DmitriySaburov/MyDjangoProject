ff6c6f34b64b296b351b8928b87e89185e32566a
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-27 01:39:53 +0300

    добавил . в импорт ключа в файле settings.py

--- MyDjangoProject/settings.py
+++ MyDjangoProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from SECRET_KEY import KEY
+from .SECRET_KEY import KEY
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
