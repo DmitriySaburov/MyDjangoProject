03dedfd1b4312daa09233ec8fe2b0300f61c215c
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-27 18:04:30 +0300

    тестировал передачу аргументов из url в views

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("user/<str:name>/<int:age>/", views.user),
+    path("user/<str:name>/", views.user),
+    path("user/", views.user),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,6 @@ def about(request):
 
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
+
+def user(request, name="Неизвестно", age="Неизвестно"):
+    return HttpResponse(f"<h2>Имя: {name}</h2><br><p>Возраст: {age}</p>")
