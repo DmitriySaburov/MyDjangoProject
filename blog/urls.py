2f94d797c13d091211e622a7f8ceeac125b9524f
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-29 14:57:46 +0300

    шаблон закинул в папку, urls и views возвращают шаблон

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('index/<int:id>/', views.index),
-    path('access/<int:age>/', views.access),
+    path("", views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest, HttpResponsePermanentRedirect
+from django.shortcuts import render
 
-def index(request, id):
-    people = [None, 'Bob', 'Sam', 'Tom']
-    # если пользователь найден, возвращаем его
-    if id in range(1, len(people)):
-        return HttpResponse(people[id])
-    # если нет, то возвращаем ошибку 404
-    else:
-        return HttpResponseNotFound('Not Found')
-
-
-def access(request, age):
-    # если возраст НЕ входит в диапазон 1-110, посылаем ошибку 400
-    if age not in range(1, 111):
-        return HttpResponseBadRequest('Некорректные данные')
-    # если возраст меньше 18, то возвращаем ошибку 403
-    if age < 18:
-        return HttpResponseForbidden('Доступ заблокирован: недостаточно лет')
-    # если нет, тогда доступ разрешен
-    else:
-        return HttpResponse('Доступ разрешен')
+def index(request):
+    return render(request, "blog/index.html")
